55a91dd2e89c7405c6b2babb611663df9a75482e
Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-10-02 13:30:37 +0500

    2nd project ongoing

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setloading] = useState(true);
+  const [tours, settours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    settours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setloading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setloading(false);
+      settours(tours);
+    } catch (error) {
+      setloading(false);
+      console.log(error);
+    }
+  };
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
-export default App
+export default App;
