9a6d4cf3d39e0fe5257469b6673f07b4421b1ab0
Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-10-07 02:32:10 +0500

    main setup

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -26,6 +26,26 @@ function App() {
     }
   };
 
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
