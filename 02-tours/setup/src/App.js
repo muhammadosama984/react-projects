5075732a9b602e76c8cfc06a4db2adc5c4ed0fb2
Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-10-23 17:54:15 +0500

    Tour Completed

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -4,28 +4,31 @@ import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
+
 function App() {
-  const [loading, setloading] = useState(true);
-  const [tours, settours] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
 
   const removeTour = (id) => {
     const newTours = tours.filter((tour) => tour.id !== id);
-    settours(newTours);
+    setTours(newTours);
   };
 
   const fetchTours = async () => {
-    setloading(true);
+    setLoading(true);
     try {
       const response = await fetch(url);
       const tours = await response.json();
-      setloading(false);
-      settours(tours);
+      setLoading(false);
+      setTours(tours);
     } catch (error) {
-      setloading(false);
+      setLoading(false);
       console.log(error);
     }
   };
-
+  useEffect(() => {
+    fetchTours();
+  }, []);
   if (loading) {
     return (
       <main>
@@ -33,7 +36,6 @@ function App() {
       </main>
     );
   }
-
   if (tours.length === 0) {
     return (
       <main>
